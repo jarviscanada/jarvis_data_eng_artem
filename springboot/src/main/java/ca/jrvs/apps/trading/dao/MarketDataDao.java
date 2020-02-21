9de986f76bb36a3bfac6ef95745caef4d3edd58d
artemanosov <aransov@protonmail.com>
2020-02-21 08:38:18 -0600

    removed .idea directories

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
@@ -0,0 +1,114 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import ca.jrvs.apps.trading.model.domain.IexQuote;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MarketDataDao implements CrudRepository<IexQuote,String> {
+  private static final String IEX_BATCH_PATH = "/stock/market/batch?symbols=%s&types=quote&token=";
+  private final String IEX_BATCH_URL;
+
+  private Logger logger = LoggerFactory.getLogger(MarketDataDao.class);
+  private HttpClientConnectionManager httpClientConnectionManager;
+
+  @Autowired
+  public MarketDataDao(HttpClientConnectionManager httpClientConnectionManager,
+      MarketDataConfig marketDataConfig) {
+    this.httpClientConnectionManager = httpClientConnectionManager;
+    IEX_BATCH_URL = marketDataConfig.getHost() + IEX_BATCH_PATH + marketDataConfig.getToken();
+  }
+
+  @Override
+  public Optional<IexQuote> findById(String ticker) {
+    Optional<IexQuote> iexQuote;
+    List<IexQuote> quotes = findAllById(Collections.singletonList(ticker));
+
+    if(quotes.size() == 0){
+      return Optional.empty();
+    }else if(quotes.size() == 1){
+      iexQuote = Optional.of(quotes.get(0));
+    }else {
+      throw new DataRetrievalFailureException("Unexpected number of quotes");
+    }
+
+    return iexQuote;
+  }
+
+  @Override
+  public List<IexQuote> findAllById(Iterable<String> tickers) {
+    String tickersString = "";
+    for(int i=0; i<tickers)
+    return null;
+  }
+
+  private Optional<String> executeHttpGet(String url){
+
+    return null;
+  }
+
+  private CloseableHttpClient getHttpClient(){
+    return HttpClients.custom()
+        .setConnectionManager(httpClientConnectionManager)
+        .setConnectionManagerShared(true)
+        .build();
+  }
+
+  @Override
+  public boolean existsById(String s) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public Iterable<IexQuote> findAll() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+
+  @Override
+  public long count() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteById(String s) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void delete(IexQuote entity) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteAll(Iterable<? extends IexQuote> entities) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteAll() {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public <S extends IexQuote> S save(S entity) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public <S extends IexQuote> Iterable<S> saveAll(Iterable<S> entities) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+}
